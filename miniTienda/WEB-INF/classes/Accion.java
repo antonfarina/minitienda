5044d4582a8b14b4c7c959e2fe3fc582c5a6c331
anton <antonio.farina@rai.usc.es>
2022-04-27 18:32:57 +0200

    compra

--- miniTienda/WEB-INF/classes/Accion.java
+++ miniTienda/WEB-INF/classes/Accion.java
@@ -1,4 +1,5 @@
-
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -18,16 +19,41 @@ public class Accion extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         // Ejecutamos en funcion de la accion del usuario
-        // Si Accion es "suma"....
-        if (request.getParameter("suma") != null) {
-            // SumaNumeros
-            gotoPage("/SumaNumeros", request, response);
-        } // Si Accion es "multiplica"...
-        else if (request.getParameter("multiplica") != null) {
-            // MultiplicaNumeros
-            gotoPage("/MultiplicaNumeros", request, response);
-        }
+        // Si Accion es "añadir al carrito"
+        if (request.getParameter("anadir") != null) {
+            //añadimos al carrito el nuevo elemento a traves de la sumaSesion
+            // Generamos un objeto sesion
+            HttpSession sesion = request.getSession(true);
 
+            String movil = request.getParameter("movil");
+            String color = request.getParameter("color");
+            Integer cantidad = Integer.parseInt(request.getParameter("cantidad"));
+
+            // Recuperamos informacion del movil
+            StringTokenizer t = new StringTokenizer(movil, "|");
+            String modelo = t.nextToken();
+            String marca = t.nextToken();
+            Integer precio = Integer.parseInt(t.nextToken().replace('\u20ac', ' ').trim());
+            Integer precioTotal = precio * Integer.parseInt(request.getParameter("cantidad"));
+
+            Ejemplar ejemplar = new Ejemplar(modelo, marca, color, precio, cantidad, precioTotal);
+            // Recuperamos sumaSesion de la sesion y si no existe, creamos este atributo
+            ArrayList<Ejemplar> carrito = (ArrayList) sesion.getAttribute("carrito");
+            if (carrito == null) {
+                System.out.println("El carrito de compra esta vacio");
+                // Inicializamos el atributo carrito
+                sesion.setAttribute("carrito", new ArrayList<>());
+                carrito = (ArrayList) sesion.getAttribute("carrito");
+            }
+            //añadimos el ejemplar al carrito
+            carrito.add(ejemplar);
+            //lo guardamos en la sesion
+            sesion.setAttribute("carrito", carrito);
+        } // Si Accion es "ver"...
+        else if (request.getParameter("ver") != null) {
+            //abrimos el carrito de la compra
+            gotoPage("/MiniTienda", request, response);
+        }
     }
 
     private void gotoPage(String address, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- miniTienda/WEB-INF/classes/MiniTienda.java
+++ miniTienda/WEB-INF/classes/MiniTienda.java
@@ -6,23 +6,13 @@ import javax.servlet.http.*;
 
 public class MiniTienda extends HttpServlet {
 
+    private Integer totalCompra=0;
+
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // Generamos un objeto sesion
         HttpSession sesion = request.getSession(true);
 
-        String movil = request.getParameter("movil");
-        String color = request.getParameter("color");
-        Integer cantidad = Integer.parseInt(request.getParameter("cantidad"));
-
-        // Recuperamos informacion del movil
-        StringTokenizer t = new StringTokenizer(movil, "|");
-        String modelo = t.nextToken();
-        String marca = t.nextToken();
-        Integer precio = Integer.parseInt(t.nextToken().replace('\u20ac', ' ').trim());
-        Integer precioTotal = precio * Integer.parseInt(request.getParameter("cantidad"));
-
-        Ejemplar ejemplar = new Ejemplar(modelo, marca, color, precio, cantidad, precioTotal);
-        // Recuperamos sumaSesion de la sesion y si no existe, creamos este atributo
+        // Recuperamos el carrito de la sesion y si no existe, creamos este atributo
         ArrayList<Ejemplar> carrito = (ArrayList) sesion.getAttribute("carrito");
         if (carrito == null) {
             System.out.println("El carrito de compra esta vacio");
@@ -30,10 +20,6 @@ public class MiniTienda extends HttpServlet {
             sesion.setAttribute("carrito", new ArrayList<>());
             carrito = (ArrayList) sesion.getAttribute("carrito");
         }
-        //añadimos el ejemplar al carrito
-        carrito.add(ejemplar);
-        //lo guardamos en la sesion
-        sesion.setAttribute("carrito", carrito);
 
         // Generamos pagina de salida
         response.setContentType("text/html");
@@ -45,28 +31,35 @@ public class MiniTienda extends HttpServlet {
                 + "<center> <H1>Compra </H1>\n"
                 + "\n\n"
                 + "<table border=\"0\" cellpadding=\"0\" width=\"75%\" bgcolor=\"#FFFFFF\">"
-                + "<th>"
-                + "<td>Marca</td>"
-                + "<td>Modelo</td>"
-                + "<td>Color</td>"
-                + "<td>Precio por unidad</td>"
-                + "<td>Cantidad</td>"
-                + "<td>Precio total</td>"
-                + "</th>";
+                + "<tr>"
+                + "<th>Marca</th>"
+                + "<th>Modelo</th>"
+                + "<th>Color</th>"
+                + "<th>Precio por unidad</th>"
+                + "<th>Cantidad</th>"
+                + "<th>Precio total</th>"
+                + "</tr>";
 
         for (Ejemplar e : carrito) {
             pagina += "<tr>"
                     + "<td>" + e.getMarca() + "</td>"
                     + "<td>" + e.getModelo() + "</td>"
                     + "<td>" + e.getColor() + "</td>"
-                    + "<td>+" + e.getPrecio() + "</td>"
+                    + "<td>" + e.getPrecio() + "</td>"
                     + "<td>" + e.getCantidad() + "</td>"
                     + "<td>" + e.getPrecioTotal() + "</td>"
                     + "</tr>";
+            totalCompra += e.getPrecioTotal();
         }
         pagina += "</table>"
+                + "<table>"
+                + "<tr>"
+                + "<td>TOTAL DE COMPRA</td>"
+                + "<td>" + totalCompra + "</td>"
+                + "</tr>"
+                + "</table>"
                 + "<p>"
-                + "<a HREF=\"/MiniTienda/index.html\">Seguir comprando</a>"
+                + "<a HREF=\"/minitienda/index.html\">Seguir comprando</a>"
                 + "</center>"
                 + "</BODY></HTML>";
 
